b85c666db21d3f847f7b51020f9f143b7d1e9ffb
spongebob888 <121923937+vincentliu77@users.noreply.github.com>
2025-12-20 22:17:42 +0800

    ci: using cloudflare.con as test server

--- scripts/test_suite/test_detection.py
+++ scripts/test_suite/test_detection.py
@@ -5,13 +5,13 @@ sp.run(["cargo","build"])
 h2 = sp.Popen(["cargo","run","--", "-c","shadowquic/config_examples/server.yaml"],
                      start_new_session=True )
 sleep(0.5)
-out = sp.run(["curl","https://echo.free.beeceptor.com:1443",
+out = sp.run(["curl","https://cloudflare.com:1443",
                "--http3-only",
                "--resolve",
                "-v",
                "--trace-time",
                "--resolve",
-               "echo.free.beeceptor.com:1443:127.0.0.1"
+               "cloudflare.com:1443:127.0.0.1"
                ],
              capture_output=True,
              text=True)
@@ -21,4 +21,4 @@ h2.terminate()
 print(out.stderr)
 
 print(out.stdout)
-assert("GET" in out.stdout)
+assert("<html>" in out.stdout)
